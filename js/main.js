c4bceb52fc04272db74db650d492619059f891ca
JohnC <real.john.chueng@gmail.com>
2021-05-03 16:36:20 +0800

    init agreement

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function displayAgreement() {
+    document.getElementById("agreement").style.display = "block";
+    document.getElementById("shadow").style.display = "block";
+}
+
+function hideAgreement() {
+    document.getElementById("agreement").style.display = "none";
+    document.getElementById("shadow").style.display = "none";
+}
