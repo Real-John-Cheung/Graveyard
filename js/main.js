0ac49915f82ee317868bb3dfa12284de42556d15
JohnC <real.john.chueng@gmail.com>
2021-05-04 15:00:08 +0800

    edit website

--- js/main.js
+++ js/main.js
@@ -7,3 +7,35 @@ function hideAgreement() {
     document.getElementById("agreement").style.display = "none";
     document.getElementById("shadow").style.display = "none";
 }
+
+// generate the website
+$(document).ready(() => {
+    getJson('./test.json');
+});
+
+function getJson(url) {
+    let request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.responseType = 'json';
+    request.onload = () => {
+        const resp = request.response;
+        if (/^2[0-9][0-9]$/.test(request.status.toString())) {
+            createGraveyard(resp);
+        } else {
+            handleLoadError()
+        }
+    };
+    request.onerror = () => {
+        console.error("Error occurs when try to fetch data");
+        handleLoadError();
+    }
+    request.send();
+}
+
+function createGraveyard() {
+    
+}
+
+function handleLoadError() {
+    $("#main").append("<p style=\"color: rgb(165,3,18); margin: auto;\"><br><br><b><i>Fail to load the data, please try to refresh the page.</i></b><p>");
+}
