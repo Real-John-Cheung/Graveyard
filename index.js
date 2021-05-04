b4f8bd324badc3e9cf0b0ea0dc06af51ce899b4a
JohnC <real.john.chueng@gmail.com>
2021-05-04 15:26:37 +0800

    edit data structure

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import createFingerPrint from "./fingerPrint.js"
 import { isOnlineNow, timeTravel } from "./findDeadSite.js"
 
 async function judgeIfDead(url) {
-    //let isOn = await isOnlineNow(url);
-    //if (isOn) return false;
+    let isOn = await isOnlineNow(url);
+    if (isOn) return false;
     let t = await timeTravel(url);
     if (typeof t !== 'object') return t;
 
     t.fingerPrint = createFingerPrint(url);
+    let d = new Date()
+    t.time = d.toUTCString();
     return t;
 }
 
