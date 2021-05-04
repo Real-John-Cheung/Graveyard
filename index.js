66fa064ba2291bd6a935955f2d755f579d586156
JohnC <real.john.chueng@gmail.com>
2021-05-04 15:12:12 +0800

    add judge if is dead

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 import createFingerPrint from "./fingerPrint.js"
 import { isOnlineNow, timeTravel } from "./findDeadSite.js"
 
-console.log(await timeTravel("http://shadksadhjksadhskad.co"));
+async function judgeIfDead(url) {
+    //let isOn = await isOnlineNow(url);
+    //if (isOn) return false;
+    let t = await timeTravel(url);
+    if (typeof t !== 'object') return t;
+
+    t.fingerPrint = createFingerPrint(url);
+    return t;
+}
+
+console.log(await judgeIfDead("https://cnn.com"))
