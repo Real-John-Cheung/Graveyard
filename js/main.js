1aa8fbc109dff0f64e0f457cb8d02f5c7ce7d947
JohnC <real.john.chueng@gmail.com>
2023-06-12 16:34:52 +0100

    change for per

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ function fetchJSON(cla) {
 
     const c = Parse.Object.extend(cla);
     const q = new Parse.Query(c);
-    q.limit(1000);
+    q.limit(100);
     q.find().then((res) => {
         let objs = [];
         res.forEach(o => {
